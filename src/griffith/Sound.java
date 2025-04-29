192ba5455f45145d06724f790b8e4339ef21a0ac
olimeh_kelvin_3112489_ws1 <olimehkelvin@gmail.com>
2025-04-30 00:43:22 +0100

    created a sound class to enable a cheer after each goal

---
+++ src/griffith/Sound.java
@@ -0,0 +1,24 @@
+
+package griffith;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+public class Sound{
+
+	
+
+	    public static void playSound(String soundFilePath) {
+	        try {
+	            File soundFile = new File(soundFilePath);
+	            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);
+	            Clip clip = AudioSystem.getClip();
+	            clip.open(audioStream);
+	            clip.start();
+	        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+	            System.err.println("Sound error: " + e.getMessage());
+	        }
+	    
+	}
+
+
+}
